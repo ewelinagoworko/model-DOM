46e34a7e3cf35fde90a18afb50da47ca60d84c31
Ewelina Goworko <ewelina.goworko@gmail.com>
2017-04-02 12:24:01 +0200

    commit

--- js/5-zdarzenia.js
+++ js/5-zdarzenia.js
@@ -8,3 +8,54 @@ function nowaFunkcja() {
 
 //onmouseover - najezdzamy myszka na paragraf i pojawia sie alert
 //onclick - po kliknieciu myszka na paragraf pojawia sie alert
+
+
+// drugi sposob wywolywania zdarzen bez zmieniania struktury html, lepiej robic to poprzez manipulacje DOM: zmienaimy tutaj kolor tła paragrafu:
+
+var parSecond = document.getElementById('parSecond');
+
+function changeBackground() {
+	parSecond.style.backgroundColor = '#00ff00';
+}
+parSecond.onclick = changeBackground;
+
+
+//teraz zmieniamy, aby na podwojne klikniecie zmienialo sie tlo paragrafu na niebieskie:
+
+
+function changeBackgroundDB() {
+	parSecond.style.backgroundColor = '#0000ff';
+}
+parSecond.ondblclick = changeBackgroundDB;
+
+
+// trzeci sposob dodawania eventow przy pomocy addEventListener - i uzywamy to wtedy, kiedy bedzie tez potrzeba usuniecia potem tego eventu
+// przepisujemy event double click przy pomocy addEventListener:
+
+parSecond.addEventListener('dblclick', changeBackgroundDB);
+
+
+//przekazywanie pseudo parametrow do funkcji, definiujemy anonimowa funkcje z parametrem e: i to pozwala zobaczyc nawet, gdzie myszka kliknela
+
+parSecond.addEventListener('click', function(e){
+	console.log(e);
+});
+
+
+//po kliknieciu w link nie przeniesie nas na strone internetowa zawarta w linku, tylko zaladuje w konsoli to wydarzenie:
+
+document.getElementById('parFirst').children[3].addEventListener('click', function(e){
+	
+	e.preventDefault();
+	
+	console.log(typeof e); //mozemy sprawdzic typ
+	
+});
+
+
+
+
+
+
+
+
